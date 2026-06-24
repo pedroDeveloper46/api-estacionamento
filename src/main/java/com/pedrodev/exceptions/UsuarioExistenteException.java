d7b75ab0f8a2d8ea0fc6313d5752a4708f8a8652
Pedro Victor <pedrovictorpro@outlook.com>
2026-06-24 01:11:50 -0300

    removendo alguns arquivos

--- src/main/java/com/pedrodev/exceptions/UsuarioExistenteException.java
+++
@@ -1,9 +0,0 @@
-package com.pedrodev.exceptions;
-
-@SuppressWarnings("serial")
-public class UsuarioExistenteException extends RuntimeException {
-
-	public UsuarioExistenteException(String message) {
-		super(message);
-	}
-}
